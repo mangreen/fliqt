b08325ab83d2dd72151a97fc5667691761e535ef
mangreen <mangreen@gmail.com>
2024-07-08 22:54:56 +0800

    Added ManagerID to User

--- pkg/http/api/user.go
+++ pkg/http/api/user.go
@@ -6,4 +6,12 @@ type UserCreateRequest struct {
 	Password   string `json:"password" binding:"required"`
 	Role       string `json:"role" binding:"required"`
 	Department string `json:"department" binding:"required"`
+	ManagerID  string `json:"manager_id"`
+}
+
+type UserUpdateRequest struct {
+	Name       string `json:"name" binding:"required"`
+	Role       string `json:"role" binding:"required"`
+	Department string `json:"department" binding:"required"`
+	ManagerID  string `json:"manager_id"`
 }
--- pkg/model/user.go
+++ pkg/model/user.go
@@ -15,6 +15,7 @@ type User struct {
 	Password   string    `gorm:"not null;size:100;" json:"-"`
 	Role       string    `gorm:"not null;size:20;" json:"role" example:"employee"`
 	Department string    `gorm:"not null;size:20;" json:"department" example:"AI"`
+	ManagerID  string    `gorm:"size:64; " json:"manager_id" example:"miss_m"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"upated_at"`
 }
